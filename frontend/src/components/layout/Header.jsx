e6bb1668a2a280b031a41c152b52bf43fe6ee7ae
Ajay Shankar <ajayshankar@Ajays-MacBook-Pro.local>
2026-06-27 14:49:27 +0530

    Logo update-3

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -122,7 +122,7 @@ export default function Header() {
           </span>
         </Link> */}
         <Link to="/" className="flex items-center gap-2 flex-shrink-0">
-          <img src={logo} alt="DevConnect" className="h-8 w-auto" />
+          <img src={logo} alt="DevConnect" className="h-12 w-auto" />
         </Link>
 
         {/* Search */}
